dffa762b0995b725141097ceb97ffdc0d6c78159
Theagle4K <salihuz419@gmail.com>
2023-11-19 23:33:24 +0100

    Fixing the 1st insertionsort

--- insertionsort.cc
+++ insertionsort.cc
@@ -8,13 +8,18 @@ void insertionsort::get_vector(std::vector<int> _unsorted_vector){
 
 }
 std::vector<int> insertionsort::i_sort(){
-	for(int l = 0; unsigned(l) <  insertionsort::unsorted_array.size();l++){
+	for(int l = 0; unsigned(l + 1) <  insertionsort::unsorted_array.size();l++){
 		int r = l + 1;
-		while(insertionsort::unsorted_array[l] > insertionsort::unsorted_array[r] && insertionsort::unsorted_array[l] >= 0 ){
+		const int _l = l;
+		const int _r = r;
+
+		while(insertionsort::unsorted_array[l] > insertionsort::unsorted_array[r] && l >= 0 ){
 				std::swap(insertionsort::unsorted_array[l],insertionsort::unsorted_array[r]);
 				l--;
 				r--;
 		}
+		l= _l;
+		r= _r;
 	}
-	return sorted_array;
+	return unsorted_array;
 }
--- main.cc
+++ main.cc
@@ -10,9 +10,15 @@ int main(){
 	std::vector<int>arangedNumbers = { 198, 43 , 97, 8 , 1 , 34 , 7 ,2 };
 	Heapsort sort;
 	someNumbers = sort.hsort_vector(someNumbers);
+	std::vector<int>somenumbers_T = { 12, 11, 13, 5, 6 };
+	std::vector<int>arangednumbers_T = { 5, 6, 11, 12, 13 };
+	insertionsort sort_T;
+	sort_T.get_vector(somenumbers_T);
+	somenumbers_T=sort_T.i_sort();
 	//Test Case 1; ARE THESE VECTORS EQUAL AFTER SORTING
 	assert(someNumbers == arangedNumbers);
-
+	//Test Case 2;
+	assert(somenumbers_T == arangednumbers_T);
 
 	return 0;
 }
