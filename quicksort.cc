47a109799ab7b47b4cedc809165a1356ccdc5004
Theagle4K <salihuz419@gmail.com>
2023-11-24 20:46:18 +0100

    Quicksort Defined

---
+++ quicksort.cc
@@ -0,0 +1,20 @@
+#ifndef QUICKOSRT_H
+#define QUICKSORT_H
+#include <vector>
+
+class Quicksort{
+private:
+	std::vector<int> _unsorted_vector;
+	std::vector<int> _sorted_vector;
+
+
+public:
+
+	void get_vector(std::vector<int> _unsorted_array);
+
+};
+
+
+
+
+#endif
